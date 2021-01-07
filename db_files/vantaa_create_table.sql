dee628f2848f214ca2cfe44750dd0da94619bef6
Koji-Sunioj <58123046+Koji-Sunioj@users.noreply.github.com>
2021-01-07 20:55:25 +0200

    Add files via upload

---
+++ db_files/vantaa_create_table.sql
@@ -0,0 +1,17 @@
+create table vantaa_weather
+(	record 	datetime,
+	temp	float,
+    dew		float,
+    humid	float,
+    precip	float,
+    msl		float,
+    clouds	float,
+    visible	float,
+    wind_speed	float,
+    wind_gust	float,
+    wind_dir 	float,
+    snow		float,
+    
+    primary key(record)
+	
+);
